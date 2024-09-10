7c5bd880198acb5c4bd0a243b81c0f7baff7ff7a
韩静静 <919979780@qq.com>
2024-09-10 16:18:13 +0800

    qianduan

--- src/views/ResultsLibrary/WorkBox/customization.vue
+++ src/views/ResultsLibrary/WorkBox/customization.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
+  <div class="main_page">
     <iframe
-      id="bid"
-      style="width: 1000px; height: 1000px"
+      id="my-iframe"
+      width="100%"
+      height="100%"
+      frameborder="0"
+      allowfullscreen
       src="http://172.28.144.167:8083/#/knowledgeBase/knowledgeBase?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIzMzI1OTUyMzIxLCJzdWIiOiIwMUg1OTVNMkM5MkJBSjM2NFNQRlpKVjFYWiJ9.8VbQliLAbpjXCrlIakW7JuLWYakAh7c2NFKiL4TFHaU"
     ></iframe>
   </div>
@@ -11,5 +14,33 @@
 <script setup>
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.main_page {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  .overlay {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    padding: 10px;
+    border-radius: 5px;
+    z-index: 1000;
+    /* 确保div在iframe上方 */
+  }
+}
+#my-iframe {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  z-index: 9;
+}
+.btn{
+  width: 100%;
+}
 </style>
